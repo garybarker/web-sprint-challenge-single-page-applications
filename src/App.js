68eff9acba26c69081363dc0214ed5774257591b
Gary Barker <79113289+garybarker@users.noreply.github.com>
2022-04-28 20:56:30 -0400

    alot of initial setup done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,74 @@
-import React from "react";
+import React, {useState} from "react";
+import {Link, Route} from "react-router-dom";
+import OrderForm from "./components/OrderForm";
+import axios from "axios";
+
+
+//initial values
+const initialFormValues = {
+  name: "",
+  size: "",
+  toppings: {
+    pepperoni: false,
+    sausage: false,
+    onion: false,
+    peppers: false
+  },
+  special: ""
+};
+
+
 
 const App = () => {
+
+  //Setting slices of state 
+  
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [newOrder, setNewOrder] = useState([]);
+  
+
+  const updateForm = (inputName, inputValue) => {
+    setFormValues({...formValues, [inputName]: inputValue});
+
+  }
+
+  const submitForm = () => {
+    const newPizza = {
+      name: formValues.name.trim(),
+      size: formValues.size.trim(),
+      toppings: formValues.toppings,
+      special: formValues.special.trim()
+    }
+
+    setNewOrder([...newOrder, newPizza]);
+    
+    axios.post("https://reqres.in/api/orders", newOrder)
+      .then(res => {
+        console.log(res.data)
+      }).catch(err => console.error(err))
+      .finally(() => setFormValues(initialFormValues))
+    }
+
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/pizza" id="order-pizza">Order</Link>
+      </nav>
+      
+      <Route exact path="/">
+        <p>Home Page</p>
+      </Route>
+      <Route exact path="/pizza">
+        <OrderForm 
+          values={formValues}
+          update={updateForm}
+          submit={submitForm}
+        />
+      </Route>
     </>
   );
 };
 export default App;
+
